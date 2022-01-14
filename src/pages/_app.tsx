30c8726f976b3808a919a17ae6eb67b0a3e3a197
RAINeko256 <reirain256@gmail.com>
2022-01-14 17:25:39 +0900

    initialize next project
    - ディレクトリ構成をAtomicDesignから分類を減らし、componentのみに
    - 絶対パスでのインポートを可能に
    import Hoge from 'common/component'
    - pages,assets,stylesなどをsrcフォルダに移動
    - chakraUIの導入

---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import type { AppProps } from 'next/app'
+
+import { ChakraProvider } from '@chakra-ui/react'
+
+import '../styles/globals.css'
+import React from 'react'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
+}
+
+export default MyApp
+
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Header from 'common/components/Header'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Header/>
+      <h1>Hello next</h1>
+    </>
+  )
+}
+
+export default Home
